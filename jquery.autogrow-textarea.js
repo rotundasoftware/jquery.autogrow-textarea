bc33a1fe54acc0b73faec9142e16768d67bb9bf6
David Beck <david@rotundasoftware.com>
2014-08-25 16:53:28 -0700

    more smartness

--- jquery.autogrow-textarea.js
+++ jquery.autogrow-textarea.js
@@ -22,7 +22,8 @@
         
 		options = $.extend( {
 			vertical: true,
-			horizontal: false
+			horizontal: false,
+			characterSlop: 0
 		}, options);
 
 		this.filter('textarea,input').each(function() {
@@ -30,7 +31,6 @@
             var $this       = $(this),
                 minHeight   = $this.height(),
 				maxHeight	= $this.attr( "maxHeight" ),
-                lineHeight  = $this.css('lineHeight'),
 				minWidth	= typeof( $this.attr( "minWidth" ) ) == "undefined" ? 0 : $this.attr( "minWidth" );
             
 			if( typeof( maxHeight ) == "undefined" ) maxHeight = 1000000;
@@ -50,14 +50,15 @@
             var characterWidth = shadow.width();
             shadow.html( '' );
             
-            var update = function() {
+            var update = function( val ) {
     
                 var times = function(string, number) {
                     for (var i = 0, r = ''; i < number; i ++) r += string;
                     return r;
                 };
                 
-                var val = this.value;
+                if( typeof val === 'undefined' ) val = this.value;
+				if( val === '' && $(this).attr("placeholder") ) val = $(this).attr("placeholder");
 				
 				if( options.vertical )
 					val = val.replace(/</g, '&lt;')
@@ -66,15 +67,22 @@
 						.replace(/\n$/, '<br/>&nbsp;')
 						.replace(/\n/g, '<br/>')
 						.replace(/ {2,}/g, function(space) { return times('&nbsp;', space.length -1) + ' '; });
-				
+				else
+					val = val.replace(/ /g, '&nbsp;');
+
 				//if( options.horizontal )
 				//	val = $.trim( val );
 				
-                shadow.html(val).css( "width", "auto" );
+				// if( $(this).prop( 'tagName' ).toUpperCase() === 'INPUT' )
+				// 	shadow.text(val).css( "width", "auto" );
+				// else
+				shadow.html(val).css( "width", "auto" );
 				
 				if( options.horizontal )
 				{
-					var newWidth = Math.max( shadow.width() + characterWidth * 3, minWidth );
+					var slopWidth = options.characterSlop * characterWidth + 2;
+
+					var newWidth = Math.max( shadow.width() + slopWidth, minWidth );
 					var maxWidth = options.maxWidth;
 					//if( typeof( maxWidth ) === "undefined" ) maxWidth = $this.parent().width() - 12; // not sure why we were doing this but seems like a bad idea. doesn't work with inline-block parents for one thing, since it is the text area that should be "pushing" them to be wider
 					if( maxWidth ) newWidth = Math.min( newWidth, maxWidth );
@@ -92,10 +100,17 @@
             };
             
             $(this)
-				.change(update)
-				.keyup(update)
-				.keydown(update)
-				.bind( "update.autogrow", update )
+				.change(function(){update.call( this );return true;})
+				.keyup(function(){update.call( this );return true;})
+				.keypress(function( event ) {
+					var val = this.value;
+					var caretInfo = _getCaretInfo( this );
+
+					var valAfterKeypress = val.slice( 0, caretInfo.start ) + String.fromCharCode( event.which ) + val.slice( caretInfo.end );
+					update.call( this, valAfterKeypress );
+					return true;
+				})
+				.bind( "update.autogrow", function(){update();} )
 				.bind( "remove.autogrow", function() {
 					shadow.remove();
 				} );
@@ -106,5 +121,52 @@
         
         return this;
     };
+
+    // comes from https://github.com/madapaja/jquery.selection/blob/master/src/jquery.selection.js
+    var _getCaretInfo = function(element){
+        var res = {
+            text: '',
+            start: 0,
+            end: 0
+        };
+
+        if (!element.value) {
+            /* no value or empty string */
+            return res;
+        }
+
+        try {
+            if (window.getSelection) {
+                /* except IE */
+                res.start = element.selectionStart;
+                res.end = element.selectionEnd;
+                res.text = element.value.slice(res.start, res.end);
+            } else if (doc.selection) {
+                /* for IE */
+                element.focus();
+
+                var range = doc.selection.createRange(),
+                    range2 = doc.body.createTextRange();
+
+                res.text = range.text;
+
+                try {
+                    range2.moveToElementText(element);
+                    range2.setEndPoint('StartToStart', range);
+                } catch (e) {
+                    range2 = element.createTextRange();
+                    range2.setEndPoint('StartToStart', range);
+                }
+
+                res.start = element.value.length - range2.text.length;
+                res.end = res.start + range.text.length;
+            }
+        } catch (e) {
+            /* give up */
+        }
+
+        return res;
+    };
+
     
 } ) );
